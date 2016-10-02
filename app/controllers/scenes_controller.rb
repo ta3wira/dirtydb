8f38e3bfcfb9ff78213a42eedd427bc41f229683
ta3wira <ta3wira@gmail.com>
2016-10-02 15:00:32 +0200

    enabling foundation

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -5,11 +5,12 @@ class ScenesController < ApplicationController
   end
 
   def show
+    @s = Scene.find_by(id: params[:id])
     @scripts = {}
     Script.by_dimensions(params[:id]).each do |v|
       @scripts[v['dimension_id']] = {} unless @scripts.has_key?(v['dimension_id'])
       @scripts[v['dimension_id']][v['sequence']] = v
     end
-    render json: @scripts
+    # render json: @scripts
   end
 end
